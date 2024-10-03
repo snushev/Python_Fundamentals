a25957746aeee159cc569f77b18adca14a125e18
snushev <s.nushev@gmail.com>
2024-10-03 21:31:52 +0300

    update

--- Functions/Functions_excercises/even_numbers.py
+++ Functions/Functions_excercises/even_numbers.py
@@ -1,2 +1,12 @@
-def even_numbers(numbers):
-    x = filter(lambda x: if x % 2 == 0)
+# Function to filter even numbers
+def get_even_numbers(numbers: str) -> list:
+    num_list = map(int, numbers.split())
+    even_numbers = list(filter(lambda x: x % 2 == 0, num_list))
+
+    return even_numbers
+
+
+
+numbers = input()
+even_numbers = get_even_numbers(numbers)
+print(even_numbers)
