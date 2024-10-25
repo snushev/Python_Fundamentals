930c3f316f5e2f42fff7cf6933dfdef01abe52ad
snushev <s.nushev@gmail.com>
2024-10-25 18:48:39 +0300

    update

--- example.py
+++ example.py
@@ -1,1 +1,22 @@
-print(8 // 3)
+class BankAccount:
+
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient funds")
+
+    def get_balance(self):
+        return self.balance
+
+    def account_info(self):
+        return f"Account owner: {self.owner}\nAccount balance: {self.balance}"
+
+
