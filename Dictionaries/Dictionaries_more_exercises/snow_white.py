a77fad036f2318de824491c6c5f6070177a3a209
snushev <s.nushev@gmail.com>
2024-11-02 22:13:42 +0200

    update

--- Dictionaries/Dictionaries_more_exercises/snow_white.py
+++ Dictionaries/Dictionaries_more_exercises/snow_white.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 dwarfs = {}
 
 while True:
@@ -11,9 +13,15 @@ while True:
 
     key = (dwarf_name, dwarf_hat_color)
 
-    if key in dwarfs:
-        if dwarf_physics > dwarfs[key][1]:
-            dwarfs[key] = dwarf_physics
-    dwarfs[key] = (dwarf_hat_color, dwarf_physics)
+    if key not in dwarfs or dwarf_physics > dwarfs[key][1]:
+        dwarfs[key] = (dwarf_hat_color, dwarf_physics)
 
-print(dwarfs)
+color_counts = Counter(color for name, (color, physics) in dwarfs.items())
+
+sorted_dwarfs = sorted(
+    dwarfs.items(),
+    key=lambda item: (-item[1][1], -color_counts[item[1][0]])
+)
+
+for (name, hat_color), (hat_color, physics) in sorted_dwarfs:
+    print(f"({hat_color}) {name} <-> {physics}")
